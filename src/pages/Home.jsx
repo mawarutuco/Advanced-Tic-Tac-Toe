0da99719c4e063b64b339c079483563ae5797cd7
mawarutuco <xul4841268@gmail.com>
2022-12-01 22:09:43 +0800

    加vh-100

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 function Home() {
   const navigate = useNavigate();
   return (
-    <div className="container vh-100 d-flex flex-column justify-content-center align-items-center ">
+    <div className="container d-flex flex-column justify-content-center align-items-center ">
       <h1>請選擇遊戲</h1>
       <MyBtn text="類估估畫畫" onClick={() => navigate("/alias-game")} />
     </div>
