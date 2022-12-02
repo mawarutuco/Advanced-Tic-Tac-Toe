86f1218b14e03289752525217f48e24cf6c20367
mawarutuco <xul4841268@gmail.com>
2022-12-02 22:00:38 +0800

    增加進階圈圈叉叉

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,10 @@ import { useNavigate } from "react-router-dom";
 function Home() {
   const navigate = useNavigate();
   return (
-    <div className="container d-flex flex-column justify-content-center align-items-center ">
+    <div className="my_show_move_up container d-flex flex-column justify-content-center align-items-center ">
       <h1>請選擇遊戲</h1>
-      <MyBtn text="類估估畫畫" onClick={() => navigate("/alias-game")} />
+      <MyBtn text="類估估畫畫" onClick={() => navigate("/alias-game")} btnClass="m-3"/>
+      <MyBtn text="進階圈圈叉叉" onClick={() => navigate("/advanced-tic-tac-toe")} btnClass="m-3" />
     </div>
   );
 }
