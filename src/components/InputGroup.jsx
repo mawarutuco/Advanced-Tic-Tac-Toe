e0fc325f9d6b7b1c2ae1f06108ba94f47e832e11
mawarutuco <xul4841268@gmail.com>
2022-12-02 21:59:25 +0800

    InputGroup裡加btn

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,11 +1,27 @@
-import { InputGroup, Form } from "react-bootstrap";
+import { InputGroup, Form, Button } from "react-bootstrap";
 
-const MyInputGroup = ({ frontText, behindText, value, onChange }) => {
+const MyInputGroup = ({
+  frontText,
+  behindText,
+  btnText,
+  btnClick,
+  btnComponent,
+  value,
+  onChange,
+}) => {
   return (
     <InputGroup>
       {frontText ? <InputGroup.Text>{frontText}</InputGroup.Text> : ""}
       <Form.Control value={value} onChange={onChange} />
       {behindText ? <InputGroup.Text>{behindText}</InputGroup.Text> : ""}
+      {btnText ? (
+        <Button className="btn-secondary" onClick={btnClick}>
+          <span>{btnText}</span>
+        </Button>
+      ) : (
+        ""
+      )}
+      {btnComponent}
     </InputGroup>
   );
 };
