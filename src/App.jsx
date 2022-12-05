a5f38ce1041517722b58d93eb7458da8e7f3cebe
于萱 <98326769+mawarutuco@users.noreply.github.com>
2022-12-05 08:06:42 +0000

    更改路由

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import AliasGame from "./pages/alias-game/index";
 // import OOXX from "./pages/advanced-tic-tac-toe/index";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/alias-game" element={<AliasGame />} />
         {/* <Route path="/advanced-tic-tac-toe" element={<OOXX />} /> */}
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
