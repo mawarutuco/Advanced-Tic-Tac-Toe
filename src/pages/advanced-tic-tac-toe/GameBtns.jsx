34a9e28c90af46b78bbea6b412be00371c57de85
mawarutuco <xul4841268@gmail.com>
2022-12-06 21:06:07 +0800

    Create GameBtns.jsx

---
+++ src/pages/advanced-tic-tac-toe/GameBtns.jsx
@@ -0,0 +1,15 @@
+import MyBtn from "../../components/Btn";
+import { ButtonGroup } from "react-bootstrap";
+import { FiCircle } from "react-icons/fi";
+
+const OOXX = ({ btnClass }) => {
+  return (
+    <ButtonGroup className="w-100">
+      <MyBtn text={<FiCircle />} textClass="my_fs_200" btnClass={btnClass} />
+      <MyBtn text={<FiCircle />} textClass="my_fs_300" btnClass={btnClass} />
+      <MyBtn text={<FiCircle />} textClass="my_fs_400" btnClass={btnClass} />
+    </ButtonGroup>
+  );
+};
+
+export default OOXX;
