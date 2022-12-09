5a3a9156696fcb5d5ad3086fc895b86b0feae367
于萱 <98326769+mawarutuco@users.noreply.github.com>
2022-12-09 03:07:52 +0000

    棋盤狀態抽出，增加手上棋子狀態，增加回合狀態

--- src/pages/advanced-tic-tac-toe/index.jsx
+++ src/pages/advanced-tic-tac-toe/index.jsx
@@ -1,17 +1,40 @@
-
 import HomeBtn from "../../components/HomeBtn";
 import GameBtns from "./GameBtns";
 import Stage from "./Stage";
+import { useState } from "react";
 
 const AdvancedTicTacToe = () => {
+  let initArr = [];
+  initArr.length = 9;
+  initArr.fill({ text: "", textClass: "", disabled: false });
+
+  const [stage, setStage] = useState(initArr);
+  const [piece, setPiece] = useState("");
+  const [turn, setTurn] = useState(true);
   return (
     <div className="container d-flex flex-column justify-content-between align-items-center mt-1">
       <div className="position-fixed fixed-top">
         <HomeBtn />
       </div>
-      <GameBtns />
-      <Stage />
-      <GameBtns btnClass="btn-warning" />
+      <GameBtns
+        turn={!turn}
+        setPiece={setPiece}
+        stage={stage}
+        setStage={setStage}
+      />
+      <Stage
+        piece={piece}
+        setTurn={setTurn}
+        stage={stage}
+        setStage={setStage}
+      />
+      <GameBtns
+        turn={turn}
+        setPiece={setPiece}
+        stage={stage}
+        setStage={setStage}
+        btnClass="btn-warning"
+      />
     </div>
   );
 };
