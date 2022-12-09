a2b3dbf4db7fa6f4676828bd24d6a1bee755c946
于萱 <98326769+mawarutuco@users.noreply.github.com>
2022-12-09 03:07:08 +0000

    重構按鈕，增加改變棋子狀態方法，增加選棋子後棋盤判斷

--- src/pages/advanced-tic-tac-toe/GameBtns.jsx
+++ src/pages/advanced-tic-tac-toe/GameBtns.jsx
@@ -2,12 +2,30 @@ import MyBtn from "../../components/Btn";
 import { ButtonGroup } from "react-bootstrap";
 import { FiCircle } from "react-icons/fi";
 
-const AdvancedTicTacToe = ({ btnClass }) => {
+const AdvancedTicTacToe = ({ turn, setPiece, stage, setStage, btnClass }) => {
+  const gameBtnsArr = ["", "my_fs_200", "my_fs_300"];
+  const selectPiece = (item) => {
+    judgeStage(item);
+    setPiece(item);
+  };
+  const judgeStage = (item) => {
+    let newStage = [];
+    if (item === "my_fs_300") console.log();
+    else if (item === "my_fs_200") console.log();
+    else console.log();
+    // setStage(newStage);
+  };
   return (
     <ButtonGroup className="w-100">
-      <MyBtn text={<FiCircle />} textClass="my_fs_200" btnClass={btnClass} />
-      <MyBtn text={<FiCircle />} textClass="my_fs_300" btnClass={btnClass} />
-      <MyBtn text={<FiCircle />} textClass="my_fs_400" btnClass={btnClass} />
+      {gameBtnsArr.map((item) => (
+        <MyBtn
+          text={<FiCircle />}
+          doClick={() => selectPiece(item)}
+          textClass={item}
+          btnClass={btnClass}
+          disabled={turn}
+        />
+      ))}
     </ButtonGroup>
   );
 };
