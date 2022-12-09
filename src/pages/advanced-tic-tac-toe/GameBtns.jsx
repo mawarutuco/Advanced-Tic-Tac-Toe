a37ccc697641c66f267a0456bca6f0f18d72b6b5
于萱 <98326769+mawarutuco@users.noreply.github.com>
2022-12-09 08:23:40 +0000

    點棋子後判斷棋盤

--- src/pages/advanced-tic-tac-toe/GameBtns.jsx
+++ src/pages/advanced-tic-tac-toe/GameBtns.jsx
@@ -19,17 +19,21 @@ const AdvancedTicTacToe = ({
     let newStage = [];
     if (item === "my_fs_300") {
       newStage = stage.map((n) => {
-        if (n.textClass.indexOf(item) < 0) n.disabled = false;
+        if (n.textClass.indexOf(item) > -1) n.disabled = true;
+        else n.disabled = false;
         return n;
       });
     } else if (item === "my_fs_200") {
       newStage = stage.map((n) => {
-        if (n.textClass.indexOf(item) < 0) n.disabled = false;
+        if (n.textClass.indexOf("my_fs_300") > -1) n.disabled = true;
+        else if (n.textClass.indexOf(item) > -1) n.disabled = true;
+        else n.disabled = false;
         return n;
       });
     } else {
       newStage = stage.map((n) => {
-        if (n.text === "") n.disabled = false;
+        if (n.text) n.disabled = true;
+        else n.disabled = false;
         return n;
       });
     }
