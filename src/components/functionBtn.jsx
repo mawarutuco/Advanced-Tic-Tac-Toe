e25f79b1258cf7a37fae4dd500507bff70a17ffe
mawarutuco <xul4841268@gmail.com>
2022-12-18 21:09:53 +0800

    增加問號按鈕、無法連續點暫停按鈕

--- src/components/functionBtn.jsx
+++ src/components/functionBtn.jsx
@@ -1,21 +1,47 @@
 import MyBtn from "./Btn";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { AiFillHome } from "react-icons/ai";
+import { FaQuestion } from "react-icons/fa";
 import { GiPauseButton } from "react-icons/gi";
+
+const btnColor = "btn-secondary";
 
 export const HomeBtn = () => {
   const navigate = useNavigate();
   return (
     <MyBtn
-      btnClass="btn-secondary"
+      btnClass={btnColor}
       text={<AiFillHome />}
       doClick={() => navigate("/")}
     />
   );
 };
 
+export const StopBtn = ({ doClick }) => {
+  let [isDisabled, setDisabled] = useState(false);
+  const stopBtnFunction = () => {
+    doClick();
+    setDisabled(true);
+    setTimeout(() => setDisabled(false), 1500);
+  };
+  return (
+    <MyBtn
+      btnClass={btnColor}
+      text={<GiPauseButton />}
+      doClick={stopBtnFunction}
+      disabled={isDisabled}
+    />
+  );
+};
 
-export const StopBtn = ({ doClick }) => (
-  <MyBtn btnClass="btn-secondary" text={<GiPauseButton />} doClick={doClick} />
-);
-
+export const QuestionBtn = () => {
+  const navigate = useNavigate();
+  return (
+    <MyBtn
+      btnClass={btnColor}
+      text={<FaQuestion />}
+      doClick={() => navigate("")}
+    />
+  );
+};
